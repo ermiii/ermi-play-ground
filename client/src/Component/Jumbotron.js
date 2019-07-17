27671e4442ff34190de078b137c64c34eabff198
Ermiyas Wondimu <vectonv@gmail.com>
2019-07-17 07:54:53 +0100

    animation retrieved

--- client/src/Component/Jumbotron.js
+++ client/src/Component/Jumbotron.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import { Jumbotron as Jumbo, Container } from "react-bootstrap";
 import styled from "styled-components";
 
-
 const Styles = styled.div`
   .jumbo {
      position: fixed;
@@ -200,27 +199,25 @@ export class Jumbotron extends Component {
   render() {
     return (
       <Styles>
-     
         <Jumbo fluid className="jumbo">
           <div className="overlay" />
           <Container>
-              <div className="content">
-                <div className="content__container">
-                  <p className="content__container__text">
-                  Hello
-    </p>
+            <div className="content">
+              <div className="content__container">
+                <p className="content__container__text">Hello</p>
 
                 <ul className="content__container__list">
                   <li className="content__container__list__item">Student!</li>
-                    <li className="content__container__list__item">Mentors!</li>
-                    <li className="content__container__list__item">Admins!</li>
-                    <li className="content__container__list__item">Welcome to CYF!</li>
+                  <li className="content__container__list__item">Mentors!</li>
+                  <li className="content__container__list__item">Admins!</li>
+                  <li className="content__container__list__item">
+                    Welcome to CYF!
+                  </li>
                 </ul>
               </div>
             </div>
           </Container>
         </Jumbo>
-    
       </Styles>
     );
   }
